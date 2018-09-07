00fe8dd2552c8e574806bcb1c708b0e76c279fec
queimel <ccampos.aviles@gmail.com>
2018-09-07 11:01:01 -0300

    agrega clase gestor BD

---
+++ src/java/model/GestorBD.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author cristian.campos
+ */
+public class GestorBD {
+    public boolean registrar(int clave, String nombre, Double precio, int cant){
+        Connection conn = null;
+        Statement stm;
+        ResultSet rs;
+        int resultUpdate = 0;
+        
+        try{
+            conn = ConexionBD.abrir();
+            stm = conn.createStatement();
+            resultUpdate = stm.executeUpdate("INSERT INTO Productos VALUES("+clave+",'"+ nombre + "'," + precio+"," +cant+ ");");
+            if( resultUpdate !=0){
+                ConexionBD.cerrar();
+                return true;
+            }else{
+                ConexionBD.cerrar();
+                return false;
+            }
+        }catch(Exception e){
+            System.out.println("error en la BBDD");
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
